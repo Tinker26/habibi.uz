4282262d756397a39a0ead20f2da08d3df8aaa55
umar <umarnurxodjayev@gmail.com>
2021-11-24 19:01:39 +0500

    gitignore

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-enor@#sw=^#8*q*q$d0syyx2hsm(%emr4i8(rj$5ik7s-pdtu$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['81.95.232.205', 'habibi.uz']
+ALLOWED_HOSTS = ['81.95.232.205', 'habibi.uz','127.0.0.1']
 # ALLOWED_HOSTS = []
 
 # Application definition
