6eb3539d74fcdd0c777cbaf28cff31c942c0c999
umar <umarnurxodjayev@gmail.com>
2021-12-01 19:53:59 +0500

    postgres qoshildi

--- project/settings.py
+++ project/settings.py
@@ -88,8 +88,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'habibi',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
