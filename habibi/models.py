87fbde3b58d9ff7e89298f57c51fbd7929d6fd12
Tinker26 <umarnurxodjayev17@gmail.com>
2021-12-19 16:38:46 +0500

    add number

--- habibi/models.py
+++ habibi/models.py
@@ -40,7 +40,7 @@ class Aloqa(models.Model):
     name = models.CharField(max_length=100)
     Email = models.EmailField()
     subject = models.CharField(max_length=300)
-    massage = models.TextField(default=0)
+    number = models.IntegerField()
     
     class Meta():
         ordering = ['name']
